5a6079df01abe9087cf7a3b4b1a8cbd5c4ad6a89
Salvoxia <Salvoxia@blindfish.info>
2024-06-04 21:15:15 +0200

    Removed go-cron > Moved setup.sh business logic in Dockerfile > Added support for multi-platform images (linux/arm64, linux/arm64/v8, linux/arm/v7, linux/amd64) > Added environment variable for setting mc --insecure flag Added environment variable for setting mc mirror --overwrite flag

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
-FROM alpine:latest
+FROM alpine:3.20
+LABEL maintainer="Salvoxia <salvoxia@blindfish.info>"
+ARG TARGETPLATFORM
+# Determine Minio Client Architecture sub-URL
+RUN case "${TARGETPLATFORM}" in \
+        "linux/amd64")    MC_ARCH=linux-amd64  ;; \
+        "linux/arm64")    MC_ARCH=linx-arm64  ;; \
+        "linux/arm64/v8") MC_ARCH=linx-arm64  ;; \
+        "linux/arm/v7")   MC_ARCH=linux-arm  ;; \
+        *) exit 1 ;; \
+    esac; \
+    wget https://dl.min.io/client/mc/release/${MC_ARCH}/mc; \
+    chmod +x ./mc
 
-LABEL maintainer="Sorena Sarabadani <sorena.sarabadani@fandogh.cloud>"
-
-ENV MC_HOST_SOURCE **None**
-ENV MC_HOST_DESTINATION **None**
-ENV SCHEDULE **None**
-
-ADD . .
-
-RUN sh setup.sh && rm setup.sh
+    ADD . .
 
 CMD ["sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -2,8 +2,58 @@
 
 set -e
 
-if [ "${SCHEDULE}" = "**None**" ]; then
-  sh backup.sh
-else
-  exec go-cron "$SCHEDULE" /bin/sh backup.sh
+# Validate SOURCE environment variables
+if [ -z "${MC_SOURCE_EP_HOST}" ]; then
+  echo "Error: You did not set the MC_SOURCE_EP_HOST environment variable."
+  exit 1
+fi
+if [ -z "${MC_SOURCE_EP_ACCESS_KEY}" ]; then
+  echo "Error: You did not set the MC_SOURCE_EP_ACCESS_KEY environment variable."
+  exit 1
+fi
+if [ -z "${MC_SOURCE_EP_SECRET_KEY}" ]; then
+  echo "Error: You did not set the MC_SOURCE_EP_SECRET_KEY environment variable."
+  exit 1
+fi
+
+# Validate DESTINATION environment variables
+if [ -z "${MC_DEST_EP_HOST}" ]; then
+  echo "Error: You did not set the MC_DEST_EP_HOST environment variable."
+  exit 1
+fi
+if [ -z "${MC_DEST_EP_ACCESS_KEY}" ]; then
+  echo "Error: You did not set the MC_DEST_EP_ACCESS_KEY environment variable."
+  exit 1
+fi
+if [ -z "${MC_DEST_EP_SECRET_KEY}" ]; then
+  echo "Error: You did not set the MC_DEST_EP_SECRET_KEY environment variable."
+  exit 1
+fi
+
+# MC arguments
+# --insecure
+MC_ARGS=""
+if [ ${MC_INSECURE} -eq 1 ]; then
+  MC_ARGS="--insecure $MC_ARGS"
+fi
+
+# MC mirror arguments
+# --overwrite
+MC_ARGS="$MC_ARGS mirror"
+if [ ${MC_MIRROR_OVERWRITE} -eq 1 ]; then
+  MC_ARGS="$MC_ARGS --overwrite"
+fi
+MC_ARGS="$MC_ARGS source destination"
+
+# Create mc aliases
+./mc alias set source "${MC_SOURCE_EP_HOST}" "${MC_SOURCE_EP_ACCESS_KEY}" "${MC_SOURCE_EP_SECRET_KEY}"
+./mc alias set destination "${MC_DEST_EP_HOST}" "${MC_DEST_EP_ACCESS_KEY}" "${MC_DEST_EP_SECRET_KEY}"
+
+
+echo "Mirroring buckets from ${MC_SOURCE_EP_HOST} to ${MC_DEST_EP_HOST}..."
+echo $MC_ARGS | xargs ./mc
+
+if [ $? != 0 ]; then
+  >&2 echo "Error in migarating files from ${MC_SOURCE_EP_HOST} to ${MC_DEST_EP_HOST}!"
+  exit 1
 fi
